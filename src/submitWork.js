3a7a07afee66ab40a333b60a631b0e674667a43f
MarcoCiaramella <marcociaramella87@gmail.com>
2024-03-02 17:07:25 +0100

    fix submitWork

--- src/submitWork.js
+++ src/submitWork.js
@@ -2,6 +2,6 @@ const {
   submitWork
 } = require('./messageContants');
 
-module.exports = (worker, job_id, extranonce2, ntime, nonce) => {
+module.exports = (worker, job_id, extranonce2, ntime, nonce, client) => {
   client.write(submitWork.replace("<worker.name>", worker).replace("<jobID>", job_id).replace("<ExtraNonce2>", extranonce2).replace("<ntime>", ntime).replace("<nonce>", nonce));
 };
